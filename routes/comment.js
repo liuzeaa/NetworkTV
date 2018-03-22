705702a28336b6ee91b13b5a7c8541e720fb4510
liuzeaa <1098367891@qq.com>
2018-03-22 17:55:15 +0800

    查询出当前时间24小时以前的数据功能完成

--- routes/comment.js
+++ routes/comment.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 var Comment = require('../schemas/comment');
 var User = require('../schemas/user');
+
 //获取评论列表
 router.post('/list',function(req,res,next){
-    Comment.find({isDelected:false}).populate('userId').exec(function(err,list){
+    var date = new Date(new Date().getTime()-1000*60*60*24)
+    Comment.find({isDelected:false,createdAt:{$gte:date}}).populate('userId').exec(function(err,list){
         if(err){
             res.send(err.message);
             return;
