9a955149d21df49e0d8b9e57e2f9f38229be9034
liuzeaa <1098367891@qq.com>
2018-08-01 09:59:01 +0800

    location支持https

--- public/js/wapCommon.js
+++ public/js/wapCommon.js
@@ -16,7 +16,7 @@ function checkLogin(){
         data:{},
         success:function(result){
             if(result.status=='2'){
-                if(location.href=='http://'+location.host+'/'){
+                if(location.href==location.protocol+'//'+location.host+'/'){
                     return;
                 }
                 location.href='/'
@@ -25,7 +25,7 @@ function checkLogin(){
                     alert('管理员请使用pc端进行添加用户！');
                     return;
                 }else{
-                    if(location.href=='http://'+location.host+'/video'){
+                    if(location.href==location.protocol+'//'+location.host+'/video'){
                         return;
                     }
                     location.href='/video'
